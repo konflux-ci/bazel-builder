9d63d0a6c2a5919e53f59579825d3ba3e7dfcd4b
Brian Cook <bcook@redhat.com>
2024-12-11 12:27:48 -0500

    wip

--- test/generate-build-task.sh
+++ test/generate-build-task.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 tkn bundle list -o yaml quay.io/konflux-ci/tekton-catalog/task-buildah-oci-ta:0.2 > buildah-build-task.yaml
 
 yq -i 'del(.spec.steps[] | select(.name == "sbom-syft-generate"))' buildah-build-task.yaml
